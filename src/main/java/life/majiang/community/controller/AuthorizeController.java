beac1fd534c677a585bf95ebc1ca25e8a4408772
wang <646327343@qq.com>
2019-12-13 18:47:39 +0800

    增加配置文->> ithub.clientã

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -8,6 +8,7 @@ import life.majiang.community.dto.AccessTokenDTO;
 import life.majiang.community.dto.GithubUser;
 import life.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,15 +19,23 @@ public class AuthorizeController {
     @Autowired // 把spring容器的实例加载到当前上下文
     private GithubProvider githubProvider;
 
-    @GetMapping("/callback") // 登录成功后返回到登录页
+    // 加载配置文件--github.client
+    @Value("${github.client.id}")
+    private String clientID;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.client.uri}")
+    private String clientUri;
+
+  @GetMapping("/callback") // 登录成功后返回到登录页
     // 接收返回后的参数 code, state
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("639f4c8d6441c6136b44");
-        accessTokenDTO.setClient_secret("024fb99f36d3ce6764d6e00eb15c9545aa284cb9");
+        accessTokenDTO.setClient_id(clientID);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(clientUri);
         accessTokenDTO.setState(state);
         githubProvider.getAccess_token(accessTokenDTO);
         String accessToken = githubProvider.getAccess_token(accessTokenDTO);
