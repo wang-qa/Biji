bc6c12e3b209e971a0097ded45e30dba753d2521
test_user <646327343@qq.com>
2020-03-23 16:59:47 +0800

    修复 userMapper @Autowired后报错无法启动 （添加MapperScan）

--- src/main/java/life/CommunityApplication.java
+++ src/main/java/life/CommunityApplication.java
@@ -4,11 +4,12 @@ package life;
  * Spring Boot 主函数入口
  */
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication //(scanBasePackages = "life.majiang.community.mapper")
+@MapperScan(value = "life.majiang.community.mapper")
 public class CommunityApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommunityApplication.class, args);
