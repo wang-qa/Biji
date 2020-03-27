71628fbea08cabb68ed355d5d21b89140c0959a1
test_user <646327343@qq.com>
2020-03-27 18:03:52 +0800

    Update: 集成 publish.html

--- src/main/java/life/majiang/community/controller/PublishController.java
+++ src/main/java/life/majiang/community/controller/PublishController.java
@@ -1,12 +1,71 @@
 package life.majiang.community.controller;
 
+import life.majiang.community.mapper.QuestionMapper;
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.Question;
+import life.majiang.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
+
+    @Autowired // 注入 questionMapper
+    private QuestionMapper questionMapper;
+
+    @Autowired // 注入 userMapper
+    private UserMapper userMapper;
+
+    // get 去渲染页面
     @GetMapping("/publish")
-    public String publish(){
+    public String publish() {
         return "publish";
+    }
+
+    // post 去执行请求
+    @PostMapping("/publish")
+    public String doQuestion(
+            //接收参数
+            @RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam("tag") String tag,
+            HttpServletRequest request,
+            Model model
+    ) {
+        // 获取填写者信息
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) { // 检查 cookies_key是否为 token
+                String token = cookie.getValue();
+                user = userMapper.findByToken(token);
+                // 如果user != null 写入session
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break; // 命中后结束循环
+            }
+        }
+
+        if (user == null) {
+            model.addAttribute("error", "用户未登录");
+            return "publish";// 有问题返回 piblish
+        }
+
+        // 获取参数
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setCreator(user.getId());
+        questionMapper.create(question);
+        return "redirect:/"; // 成功返回 首页
     }
 }
---
+++ src/main/java/life/majiang/community/model/Question.java
@@ -0,0 +1,94 @@
+package life.majiang.community.model;
+
+public class Question {
+    private Integer id; // 问题自增id
+    private String title; // title
+    private String description; // 描述
+    private String tag; // 标签
+    private Long gmtCreate; // 创建时间
+    private Long gmtModified; // 更新时间
+    private Integer creator; // 创建者
+    private Integer viewCount; // 阅读数
+    private Integer commentCount; // 提交数
+    private Integer likeCount; // 点赞数
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Long getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Long gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Long getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Long gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+    public Integer getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Integer creator) {
+        this.creator = creator;
+    }
+
+    public Integer getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(Integer viewCount) {
+        this.viewCount = viewCount;
+    }
+
+    public Integer getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(Integer commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    public Integer getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(Integer likeCount) {
+        this.likeCount = likeCount;
+    }
+}
